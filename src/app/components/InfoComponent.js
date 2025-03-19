d142a5cdffc3785ca9505d6a4243ef4c0cc41bd6
Cameron Hilliard <cameronhilliard@outlook.com>
2025-03-19 14:41:32 -0300

    commit

--- src/app/components/InfoComponent.js
+++ src/app/components/InfoComponent.js
@@ -3,7 +3,8 @@ import * as motion from "motion/react-client"
 
 const InfoComponent = (props) => {
   return (
-    <div className='lg:w-2/3 w-11/12 h-2/4 mt-8 p-8 bg-card shadow-lg rounded-lg shadow-glow/50'
+    <motion.div   
+    className='lg:w-2/3 w-11/12 h-2/4 mt-8 p-8 bg-card shadow-lg rounded-lg shadow-glow/50'
 
 
 >
@@ -11,7 +12,7 @@ const InfoComponent = (props) => {
     <h2 className='lg:text-2xl text-lg font-bold font-outfit'>{props.prompts}</h2>
     <p className='text-lg mt-4'>{props.response}</p>
 </div>
-</div>
+</motion.div>
   )
 }
 
